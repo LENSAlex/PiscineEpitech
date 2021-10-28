0ee5c244429de3d554d3a7a887656158e7a0d799
Lens Alex <alex.lens@etu.unice.fr>
2021-10-28 10:00:37 +0200

    Add boostrap lib and feature delete for user

--- deuxiemeproject/src/components/Users.vue
+++ deuxiemeproject/src/components/Users.vue
@@ -1,42 +1,64 @@
 <template>
-<div>
-  <p>test</p>
-  <button v-on:click="getUser()">Discover User</button> 
-  <ul>
-    <li v-for="user in users" :key="user.id">
-      <p>Email : {{ user.email }}</p>
-      <p>Username : {{ user.username }}</p>
-    </li>
-  </ul>
+  <div class="container">
+    <button v-on:click="getUser()">Discover User</button> 
 
-  <button v-on:click="createUser()">Create a user</button> 
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Uername</th>
+          <th scope="col">Email</th>
+          <th scope="col">Delete</th>
+          <th scope="col">Update</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in users" :key="user.email">
+            <th>{{ user.id }}</th>
+            <th>{{ user.email }}</th>
+            <th>{{ user.username }}</th>
+            <th><button type="button" class="btn btn-danger" v-on:click="DeleteUser(user.id)">Delete</button></th>
+            <th><button v-on:click="updateUserDisplay(user.id)" :data-id=user.id class="btn btn-warning">Update a user</button></th>
+        </tr>
+      </tbody>
+    </table>
+
+    <form id="DisplayPutUser" @submit.prevent="UpdateUser(user.id)" method="post" v-bind:data-id={idUserPut} class="FormulairePutUser" v-show="false">
+      <p>
+        <label for="name">Username</label>
+        <input id="name" v-model="name" type="text" name="name">
+      </p>
+      <p>
+        <label for="email">Email</label>
+        <input id="email" v-model="email" type="email" name="email">
+      </p>
+      <p>
+        <input type="submit" value="Submit">
+      </p>
+    </form>
 
 
 
-  <form id="app" @submit.prevent="SubmitUser" action="https://vuejs.org/" method="post">
-  <p>
-    <label for="name">Username</label>
-    <input id="name" v-model="name" type="text" name="name">
-  </p>
-  <p>
-    <label for="email">Email</label>
-    <input id="email" v-model="email" type="email" name="email">
-  </p>
-  <p>
-    <input type="submit" value="Submit">
-  </p>
-</form>
-
-
-
-
-
-
-
-</div>
+    <form id="app" @submit.prevent="SubmitUser" action="https://vuejs.org/" method="post">
+      <p>
+        <label for="name">Username</label>
+        <input id="name" v-model="name" type="text" name="name">
+      </p>
+      <p>
+        <label for="email">Email</label>
+        <input id="email" v-model="email" type="email" name="email">
+      </p>
+      <p>
+        <input type="submit" value="Create user" class="btn btn-primary">
+      </p>
+    </form>
+  </div>
 </template>
 
 <script>
+
+
+
 import axios from 'axios';
 
 export default {
@@ -48,6 +70,22 @@ export default {
     SubmitUser()
     {
       this.createUser(this.name , this.email)
+    },
+
+    updateUserDisplay(id)
+    {
+      alert(id)
+      this.idUserPut = id;
+    },
+
+    UpdateUser(id)
+    {
+      this.updateUser(this.name , this.email , id)
+    },
+
+    DeleteUser(id)
+    {
+      this.deleteUser(id)
     },
 
     createUser (username , email) {
@@ -62,7 +100,7 @@ export default {
       });
     },
 
-    deleteUser ({userId}) {
+    deleteUser (userId) {
       axios.delete(`http://localhost:4000/api/users/${userId}`).then(response => (this.users = response))
       .catch(error => {
         console.log(error.response)
@@ -74,13 +112,19 @@ export default {
       console.log(this.users)
     },
 
-    updateUser ({userId}) {
-      axios.put(`http://localhost:4000/api/users/${userId}`).then(response => (this.users = response))
+    updateUser (userId , username , email) {
+      axios.put(`http://localhost:4000/api/users/${userId}` ,{
+        "user":{
+          "username" : username ,
+          "email": email
+        }
+      }).then(response => (this.users = response))
     }
   },
   data () {
     return {
-      users: ''
+      users: '',
+      idUserPut : 52,
     }
   }
 }
